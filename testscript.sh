bab95996c3a8cc465444ac43d594ea65d2b482ee
shobhitsperfect <38579091+shobhitsperfect@users.noreply.github.com>
2019-01-28 21:28:51 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -4,5 +4,5 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "this is another test"
 echo "welcometo the jenkins world"
-echo "another test"
+echo "--another test--"
 echo "this was run automagically"
