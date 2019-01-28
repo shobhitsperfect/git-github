22d4f77460d3568ba75fe04407268a7f23733071
shobhitsperfect <38579091+shobhitsperfect@users.noreply.github.com>
2019-01-28 22:23:01 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,7 +2,6 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "this is another test"
 echo "welcometo the jenkins world"
 echo "-->>>another test<<<--"
 echo "this was run automagically"
